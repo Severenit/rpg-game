346c7f8b738b91b097e7daca7ae5c8875120f8af
Zar Zakharov <severenit@gmail.com>
2021-07-08 10:05:11 +0300

    Finish

--- src/client/ClientGame.js
+++ src/client/ClientGame.js
@@ -1,5 +1,8 @@
 import ClientEngine from './ClientEngine';
+import ClientWorld from './ClientWorld';
+
 import sprites from '../configs/sprites';
+import levelCfg from '../configs/world.json';
 
 class ClientGame {
     constructor(cfg) {
@@ -8,6 +11,7 @@ class ClientGame {
         });
 
         this.engine = this.createEngine();
+        this.map = this.createWorld();
         this.initEngine();
     }
 
@@ -15,12 +19,16 @@ class ClientGame {
         return new ClientEngine(document.getElementById(this.cfg.tagId));
     }
 
+    createWorld() {
+        return new ClientWorld(this, this.engine, levelCfg);
+    }
+
     initEngine() {
         this.engine
             .loadSprites(sprites)
             .then(() => {
                 this.engine.on('render', (_, time) => {
-
+                    this.map.init();
                 });
                 this.engine.start();
             });
