9852f1ed85033addfc4078f0bc26502961427330
Zar Zakharov <severenit@gmail.com>
2021-07-08 09:36:26 +0300

    test

---
+++ src/client/ClientEngine.js
@@ -0,0 +1,68 @@
+import EventSourceMixin from '../common/EventSourceMixin';
+
+class ClientEngine {
+    constructor(canvas) {
+        console.log(canvas);
+
+        Object.assign(this, {
+            canvas,
+            ctx: null,
+            imageLoaders: [],
+            sprites: {},
+            images: {},
+        });
+
+        this.ctx = canvas.getContext('2d');
+
+        this.loop = this.loop.bind(this);
+    }
+
+    start() {
+        this.loop();
+    }
+
+    loop(timestamp) {
+        const {ctx, canvas} = this;
+        ctx.fillStyle = 'black';
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        this.trigger('render', timestamp);
+        
+        this.initNextFrame();
+    }
+
+    initNextFrame() {
+        window.requestAnimationFrame(this.loop);
+    }
+
+    loadSprites(spritesGroup) {
+        this.imageLoaders = [];
+
+        for (let groupName in spritesGroup) {
+            const group = spritesGroup[groupName];
+            this.sprites[groupName] = group;
+
+            for (let spriteName in group) {
+                const {img} = group[spriteName];
+                if (!this.images[img]) {
+                    this.imageLoaders.push(this.loadImage(img));
+                }
+            }
+        }
+
+        return Promise.all(this.imageLoaders);
+    }
+
+    loadImage(url) {
+        return new Promise(resolve => {
+            const i = new Image();
+            this.images[url] = i;
+            i.onload = () => resolve(i);
+            i.src = url;
+        })
+    }
+}
+
+Object.assign(ClientEngine.prototype, EventSourceMixin);
+
+export default ClientEngine;
---
+++ src/client/ClientGame.js
@@ -0,0 +1,37 @@
+import ClientEngine from './ClientEngine';
+import sprites from '../configs/sprites';
+
+class ClientGame {
+    constructor(cfg) {
+        Object.assign(this, {
+            cfg,
+        });
+
+        this.engine = this.createEngine();
+        this.initEngine();
+    }
+
+    createEngine() {
+        return new ClientEngine(document.getElementById(this.cfg.tagId));
+    }
+
+    initEngine() {
+        this.engine
+            .loadSprites(sprites)
+            .then(() => {
+                this.engine.on('render', (_, time) => {
+
+                });
+                this.engine.start();
+            });
+    }
+
+    static init(cfg) {
+        if (!ClientGame.game) {
+            ClientGame.game = new ClientGame(cfg);
+            console.log('Game INIT');
+        }
+    }
+}
+
+export default ClientGame;
