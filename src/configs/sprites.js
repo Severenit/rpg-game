4bc42438ffd37b575c65902fc408b3f4b38b2cde
Zar Zakharov <severenit@gmail.com>
2021-07-27 17:25:48 +0300

    Video 1

--- src/configs/sprites.js
+++ src/configs/sprites.js
@@ -5,36 +5,24 @@ export default {
   terrain: {
     grass: {
       img: terrainIMG,
-      frames: [[896, 256, 64, 64]],
+      frames: [[896, 256, 64, 64]]
     },
     water: {
       img: terrainIMG,
-      frames: [[0, 576, 64, 64]],
+      frames: [[0, 576, 64, 64]]
     },
     wall: {
       img: terrainIMG,
-      frames: [[448, 64, 64, 64]],
+      frames: [[448, 64, 64, 64]]
     },
     spawn: {
       img: terrainIMG,
-      frames: [[384, 640, 64, 64]],
+      frames: [[384, 640, 64, 64]]
     },
     npcSpawn: {
       img: terrainIMG,
-      frames: [[896, 576, 64, 64]],
-    },
-    heightGrass: {
-      img: terrainIMG,
-      frames: [[904, 320, 64, 64]],
-    },
-    earthWithGrass: {
-      img: terrainIMG,
-      frames: [[192, 0, 64, 64]],
-    },
-    earth: {
-      img: terrainIMG,
-      frames: [[128, 0, 64, 64]],
-    },
+      frames: [[896, 576, 64, 64]]
+    }
   },
   characters: {
     girl1: {
