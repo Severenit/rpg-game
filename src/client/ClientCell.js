8da3f93aa43b21080c0fd8d9ae99c950e12b5104
Zar Zakharov <severenit@gmail.com>
2021-08-24 23:05:17 +0300

    Finish

--- src/client/ClientCell.js
+++ src/client/ClientCell.js
@@ -3,88 +3,88 @@ import ClientGameObject from './ClientGameObject';
 import ClientPlayer from './ClientPlayer';
 
 class ClientCell extends PositionedObject {
-  constructor(cfg) {
-    super();
-    const { cellWidth, cellHeight } = cfg.world;
+    constructor(cfg) {
+        super();
+        const {cellWidth, cellHeight} = cfg.world;
 
-    Object.assign(
-      this,
-      {
-        cfg,
-        objects: [],
-        x: cellWidth * cfg.cellCol,
-        y: cellWidth * cfg.cellRow,
-        width: cellWidth,
-        height: cellHeight,
-        col: cfg.cellCol,
-        row: cfg.cellRow,
-        objectClasses: {
-          player: ClientPlayer,
-        },
-      },
-      cfg,
-    );
+        Object.assign(
+            this,
+            {
+                cfg,
+                objects: [],
+                x: cellWidth * cfg.cellCol,
+                y: cellWidth * cfg.cellRow,
+                width: cellWidth,
+                height: cellHeight,
+                col: cfg.cellCol,
+                row: cfg.cellRow,
+                objectClasses: {
+                    player: ClientPlayer,
+                },
+            },
+            cfg,
+        );
 
-    this.initGameObjects();
-  }
+        this.initGameObjects();
+    }
 
-  initGameObjects() {
-    const { cellCfg, objectClasses } = this;
+    initGameObjects() {
+        const {cellCfg, objectClasses} = this;
 
-    this.objects = cellCfg.map((layer, layerId) =>
-      layer.map((objCfg) => {
-        let ObjectClass;
+        this.objects = cellCfg.map((layer, layerId) =>
+            layer.map((objCfg) => {
+                let ObjectClass;
 
-        if (objCfg.class) {
-          ObjectClass = objectClasses[objCfg.class];
-        } else {
-          ObjectClass = ClientGameObject;
+                if (objCfg.class) {
+                    ObjectClass = objectClasses[objCfg.class];
+                } else {
+                    ObjectClass = ClientGameObject;
+                }
+
+                return new ObjectClass({
+                    cell: this,
+                    objCfg,
+                    layerId,
+                    playerName: this.world.game.cfg?.playerName,
+                });
+            }),
+        );
+    }
+
+    render(time, layerId) {
+        const {objects} = this;
+
+        if (objects[layerId]) {
+            objects[layerId].forEach((obj) => obj.render(time));
+        }
+    }
+
+    addGameObject(objToAdd) {
+        const {objects} = this;
+
+        if (objToAdd.layerId === undefined) {
+            objToAdd.layerId = objects.length;
         }
 
-        return new ObjectClass({
-          cell: this,
-          objCfg,
-          layerId,
-          playerName: this.world.game.cfg?.playerName,
-        });
-      }),
-    );
-  }
+        if (!objects[objToAdd.layerId]) {
+            objects[objToAdd.layerId] = [];
+        }
 
-  render(time, layerId) {
-    const { objects } = this;
-
-    if (objects[layerId]) {
-      objects[layerId].forEach((obj) => obj.render(time));
-    }
-  }
-
-  addGameObject(objToAdd) {
-    const { objects } = this;
-
-    if (objToAdd.layerId === undefined) {
-      objToAdd.layerId = objects.length;
+        objects[objToAdd.layerId].push(objToAdd);
     }
 
-    if (!objects[objToAdd.layerId]) {
-      objects[objToAdd.layerId] = [];
+    removeGameObject(objToRemove) {
+        const {objects} = this;
+        objects.forEach((layer, layerId) => (objects[layerId] = layer.filter((obj) => obj !== objToRemove)));
     }
 
-    objects[objToAdd.layerId].push(objToAdd);
-  }
+    findObjectsByType(type) {
+        let foundObjects = [];
 
-  removeGameObject(objToRemove) {
-    const { objects } = this;
-    objects.forEach((layer, layerId) => (objects[layerId] = layer.filter((obj) => obj !== objToRemove)));
-  }
+        this.objects.forEach((layer) => (foundObjects = [...foundObjects, ...layer].filter((obj) => obj.type === type)));
 
-  findObjectsByType(type) {
-    let foundObjects = [];
-
-    this.objects.forEach((layer) => (foundObjects = [...foundObjects, ...layer].filter((obj) => obj.type === type)));
-
-    return foundObjects;
-  }
+        return foundObjects;
+    }
 }
 
 export default ClientCell;
